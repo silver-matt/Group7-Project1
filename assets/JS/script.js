b1c3a5c91cf376f5bee301c941707b9868b95677
Matt Silver <Mattsilver54@yahoo.com>
2022-05-02 19:41:56 -0700

    added js

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,13 +1,17 @@
 var food = "popular";
+var dataHit = "";
 const foodKey = "0042429a5d9430ed059f828e86fb5761";
 const recipeId = "f44e018b";
 
 var drinksApi =
   "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita";
 fetch(drinksApi).then(function (data) {
+  return data.json();
   console.log(data);
+}).then(function (data) {
   var drinkEl = document.createElement("p");
 //   need to add content
+
   drinkEl.textContent = "";
   var test = document.getElementById("finders");
   test.append(drinkEl);
@@ -19,4 +23,36 @@ fetch(foodApi).then(function (data) {
 return data.json();
 }).then(function (data) {
   console.log(data);
+  dataHit = data
 });
+
+// **DELETE LATER**dummy ingredient list to set up html
+var dummyArr = ["ingredient 1", "ingredient 2", "ingredient 3", "ingredient 4", "ingredient 5"];
+
+function callFoodAPI(){
+  //call correct API, get data, and pass it to createIngrList()
+  console.log("food button clicked");
+  console.log($(this));
+  var foodInput = $(this).parent().siblings("input").val();
+  console.log(foodInput);
+}
+function callDrinkAPI(){
+  //call correct API, get data, and pass it to createIngrList()
+  console.log("drink button clicked");
+  console.log($(this));
+  var drinkInput = $(this).parent().siblings("input").val();
+  console.log(drinkInput);
+}
+function createIngrList(){
+  //get data from the fetch and create an array with each ingredient being a separate element
+  //call display functions 
+}
+function displayImage(){
+  
+}
+function displayIngrList(){
+
+}
+
+$("#btnFood").on("click", "button", callFoodAPI);
+$("#btnDrink").on("click", "button", callDrinkAPI);
